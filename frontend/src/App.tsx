9baf017834abf935d540dc5b329b1860ef8da39e
KYBEX <artm.22@internet.ru>
2024-07-22 18:07:23 +0300

    Update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,18 @@
+import Cookies from 'js-cookie';
+import { useEffect, useState } from 'react';
+
 export default function App() {
+    const [path, setPath] = useState();
+    const [checkPath, setCheckPath] = useState();
+
+    useEffect(() => {
+        setCheckPath(Cookies.get("path"));
+    }, []);
+
+    if (checkPath) {
+        setPath(checkPath);
+    }
+
     return (
         <>
         <h1>hi</h1>
