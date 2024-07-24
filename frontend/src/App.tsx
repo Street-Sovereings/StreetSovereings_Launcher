f97b2a1ba0d0c85c15ec2d2694003cb287a81655
KYBEX <artm.22@internet.ru>
2024-07-24 08:59:44 +0300

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,38 @@
 import Cookies from 'js-cookie';
 import { useEffect, useState } from 'react';
+import './styles/App.scss';
+import { RunExecutable } from '../wailsjs/go/main/App';
 
 export default function App() {
-    const [path, setPath] = useState();
-    const [checkPath, setCheckPath] = useState();
+    const [path, setPath] = useState<string | undefined>(undefined);
+    const [checkPath, setCheckPath] = useState<string | undefined>(undefined);
 
     useEffect(() => {
-        setCheckPath(Cookies.get("path"));
+        setCheckPath(Cookies.get("path") || undefined);
     }, []);
 
     if (checkPath) {
         setPath(checkPath);
+        console.log("path: ", checkPath);
+    } else {
+        console.log("path: Nil");
     }
 
     return (
         <>
-        <h1>hi</h1>
+          <div className="app">
+      <div className="sidebar">
+        <button className="sidebar-button">Set Path</button>
+        <button className="sidebar-button">Launch Settings</button>
+        <button className="sidebar-button">Reset Config</button>
+        <button className="sidebar-button">Open config.json</button>
+        <button className="sidebar-button delete-button">Delete from device</button>
+      </div>
+      <div className="main">
+        <h1>Street Sovereigns</h1>
+        <button className="launch-button" onClick={() => RunExecutable(path || '')}>Launch</button>
+      </div>
+    </div>
         </>
     )
 }
